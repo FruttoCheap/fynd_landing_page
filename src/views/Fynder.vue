feeae24431c8566e6ad29aba5b6371889301218d
Frutto <s282098@studenti.polito.it>
2025-02-15 14:25:57 +0100

    merge con faq e fynder

---
+++ src/views/Fynder.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="fynder">
+        <h1 class="text-5xl font-extrabold mx-6">Scopri che <span class="text-orange-400">Fynder</span> sei!</h1>
+        <div class="mx-12 mt-9">
+            <div>Putroppo dovrai ancora aspettare per scoprire che <span class="font-bold">Fynder</span> sei...</div>
+            <br>
+            <div>Nel frattempo, lasciaci la tua mail e ti contatteremo non appena sarà pronto!</div>
+        </div>
+        <Email />
+    </div>
+</template>
+
+<script setup lang="ts">
+import Email from "../components/Email.vue";
+</script>
+
+<style scoped></style>
